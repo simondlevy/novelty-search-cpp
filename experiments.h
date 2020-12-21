9d8b2b60afb0bce0daa44fe785279b29c1d7db1d
simondlevy <simon.d.levy@gmail.com>
2020-12-21 01:49:02 -0500

    Cleanup

--- experiments.h
+++ experiments.h
@@ -55,63 +55,64 @@ int pole2_realtime_loop(Population *pop, CartPole *thecart);
 
 
 class CartPole {
-public:
-  CartPole(bool randomize,bool velocity);
-  virtual void simplifyTask();  
-  virtual void nextTask();
-  virtual double evalNet(Network *net,int thresh);
-  double maxFitness;
-  bool MARKOV;
+    public:
+        CartPole(bool randomize,bool velocity);
+        virtual ~CartPole();
+        virtual void simplifyTask();  
+        virtual void nextTask();
+        virtual double evalNet(Network *net,int thresh);
+        double maxFitness;
+        bool MARKOV;
 
-  bool last_hundred;
-  bool nmarkov_long;  //Flag that we are looking at the champ
-  bool generalization_test;  //Flag we are testing champ's generalization
+        bool last_hundred;
+        bool nmarkov_long;  //Flag that we are looking at the champ
+        bool generalization_test;  //Flag we are testing champ's generalization
 
-  double state[6];
+        double state[6];
 
-  double jigglestep[1000];
+        double jigglestep[1000];
 
-protected:
-  virtual void init(bool randomize);
+    protected:
+        virtual void init(bool randomize);
 
-private:
+    private:
 
-  void performAction(double output,int stepnum);
-  void step(double action, double *state, double *derivs);
-  void rk4(double f, double y[], double dydx[], double yout[]);
-  bool outsideBounds(); 
+        void performAction(double output,int stepnum);
+        void step(double action, double *state, double *derivs);
+        void rk4(double f, double y[], double dydx[], double yout[]);
+        bool outsideBounds(); 
 
-  static const int NUM_INPUTS=7;
-  static constexpr double MUP = 0.000002;
-  static constexpr double MUC = 0.0005;
-  static constexpr double GRAVITY= -9.8;
-  static constexpr double MASSCART= 1.0;
-  static constexpr double MASSPOLE_1= 0.1;
+        static const int NUM_INPUTS=7;
+        static constexpr double MUP = 0.000002;
+        static constexpr double MUC = 0.0005;
+        static constexpr double GRAVITY= -9.8;
+        static constexpr double MASSCART= 1.0;
+        static constexpr double MASSPOLE_1= 0.1;
 
-  static constexpr double LENGTH_1= 0.5;		  /* actually half the pole's length */
+        static constexpr double LENGTH_1= 0.5;		  /* actually half the pole's length */
 
-  static constexpr double FORCE_MAG= 10.0;
-  static constexpr double TAU= 0.01;		  //seconds between state updates 
+        static constexpr double FORCE_MAG= 10.0;
+        static constexpr double TAU= 0.01;		  //seconds between state updates 
 
-  static constexpr double one_degree= 0.0174532;	/* 2pi/360 */
-  static constexpr double six_degrees= 0.1047192;
-  static constexpr double twelve_degrees= 0.2094384;
-  static constexpr double fifteen_degrees= 0.2617993;
-  static constexpr double thirty_six_degrees= 0.628329;
-  static constexpr double fifty_degrees= 0.87266;
+        static constexpr double one_degree= 0.0174532;	/* 2pi/360 */
+        static constexpr double six_degrees= 0.1047192;
+        static constexpr double twelve_degrees= 0.2094384;
+        static constexpr double fifteen_degrees= 0.2617993;
+        static constexpr double thirty_six_degrees= 0.628329;
+        static constexpr double fifty_degrees= 0.87266;
 
-  double LENGTH_2;
-  double MASSPOLE_2;
-  double MIN_INC;
-  double POLE_INC;
-  double MASS_INC;
+        double LENGTH_2;
+        double MASSPOLE_2;
+        double MIN_INC;
+        double POLE_INC;
+        double MASS_INC;
 
-  //Queues used for Gruau's fitness which damps oscillations
-  int balanced_sum;
-  double cartpos_sum;
-  double cartv_sum;
-  double polepos_sum;
-  double polev_sum;
+        //Queues used for Gruau's fitness which damps oscillations
+        int balanced_sum;
+        double cartpos_sum;
+        double cartv_sum;
+        double polepos_sum;
+        double polev_sum;
 
 
 
